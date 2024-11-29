72b508f968db73671e000d456dfb84afd8544000
batmania <vagartmour@gmail.com>
2024-11-29 22:19:38 +0200

    Add Dates

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 habits = ["Walk the dog", "Walk with the dog"]
 
+
+def date_range(start: datetime.date):
+    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+    return dates
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", habits=habits, title="Habit Traker - Home")
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+    return render_template(
+        "index.html",
+        habits=habits,
+        title="Habit Traker - Home",
+        date_range=date_range,
+        selected_date=selected_date,
+    )
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -16,4 +34,4 @@ def add_habit():
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(port=5003)
